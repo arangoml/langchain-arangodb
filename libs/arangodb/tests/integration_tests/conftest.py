a835d8ba6a6933fbc7f423a687069d674c2ac711
Anthony Mahanna <anthony.mahanna@arangodb.com>
2025-04-28 10:43:12 -0400

    fix: lint

--- libs/arangodb/tests/integration_tests/conftest.py
+++ libs/arangodb/tests/integration_tests/conftest.py
@@ -1,7 +1,9 @@
 import os
+from typing import Generator
 
 import pytest
 from arango import ArangoClient
+from arango.database import StandardDatabase
 
 from tests.integration_tests.utils import ArangoCredentials
 
@@ -9,8 +11,9 @@ url = os.environ.get("ARANGO_URL", "http://localhost:8529")
 username = os.environ.get("ARANGO_USERNAME", "root")
 password = os.environ.get("ARANGO_PASSWORD", "test")
 
+
 @pytest.fixture
-def clear_arangodb_database():
+def clear_arangodb_database() -> None:
     client = ArangoClient(url)
     db = client.db(username=username, password=password, verify=True)
 
@@ -25,16 +28,22 @@ def clear_arangodb_database():
 
 
 @pytest.fixture(scope="session")
-def arangodb_credentials():
+def arangodb_credentials() -> ArangoCredentials:
     return {
         "url": url,
         "username": username,
         "password": password,
     }
 
+
 @pytest.fixture(scope="session")
-def db(arangodb_credentials: ArangoCredentials):
+def db(
+    arangodb_credentials: ArangoCredentials,
+) -> Generator[StandardDatabase, None, None]:
     client = ArangoClient(arangodb_credentials["url"])
-    db = client.db(username=arangodb_credentials["username"], password=arangodb_credentials["password"])
+    db = client.db(
+        username=arangodb_credentials["username"],
+        password=arangodb_credentials["password"],
+    )
     yield db
     client.close()
