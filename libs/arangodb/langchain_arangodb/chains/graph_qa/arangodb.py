44904828b59e05fed4a42c3b194df4c68e8d8bd4
MonikaLiu <anitaliubfsu@gmail.com>
2025-07-30 17:02:53 -0700

    handle edge cases for cache_query

--- libs/arangodb/langchain_arangodb/chains/graph_qa/arangodb.py
+++ libs/arangodb/langchain_arangodb/chains/graph_qa/arangodb.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 import re
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from arango import AQLQueryExecuteError, AQLQueryExplainError
 from langchain.chains.base import Chain
